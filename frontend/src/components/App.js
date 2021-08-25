73194f31ffdc4827c6cd8b75a0299f8209955676
Nadav Lev zohar <nadav@Nadavs-MacBook-Pro.local>
2021-08-26 02:08:29 +0300

    First commit

---
+++ frontend/src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ThemeProvider, sportsbetLight } from "@heathmont/moon-themes";
+import { useState } from "react";
+import { Switch } from "@heathmont/moon-components";
+
+const App = () => {
+  const [state, setState] = useState(true);
+  return (
+    <ThemeProvider theme={sportsbetLight}>
+      <main>
+        <Switch checked={state} onChange={(e) => setState(e.target.checked)} />
+      </main>
+    </ThemeProvider>
+  );
+};
+
+export default App;
